be7c4c7a71460549d85f6eab0453bf2087ef0ad4
Sirdmu <din.siraj7@gmail.com>
2021-09-18 21:15:57 +0000

    Implementing Map fucntion

---
+++ map.js
@@ -0,0 +1,23 @@
+const words = ["ground", "control", "to", "major", "tom"];
+
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+}
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// assertEqual("Lighthouse Labs", "Bootcamp");
+// assertEqual(1, 1);
+
+const results1 = map(words, word => word[0]);
+console.log(assertEqual(results1));
