61046569416c9b7c1fead7acc6ce429ccab05357
Sirdmu <din.siraj7@gmail.com>
2021-09-21 02:32:16 +0000

    Worked on Refactoring code from Lotide project

--- head.js
+++ head.js
@@ -1,20 +1,7 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 const head = function(num1) {
   return num1[0];
 };
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-
-assertEqual(head([]),);
-
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-
+module.exports = head;
